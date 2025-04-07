be05c1b3adb2a0ea7273b0257330c8e54e572f55
alecxzh <42911602+alecxzh@users.noreply.github.com>
2025-04-06 20:42:37 -0400

    Update docker_run.sh
    altered to work without a linux computer. (took out tmux)

--- scripts/docker_run.sh
+++ scripts/docker_run.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
-
-session_name=parcel2d
-# kill old session if exists
-tmux kill-session -t "${session_name}"
-sleep 1
-
-# start new tmux session
-tmux new-session -s "${session_name}" 'docker run -it --rm --name parcel2d \
+docker run -it --rm --name parcel2d \
 --mount type=bind,source=${PWD},target=/app \
 --entrypoint /bin/bash \
- parcel2d_cnn:latest'
+parcel2d_cnn:latest
